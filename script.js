90b513fc5846bd73c48f5c0f824e9f4918b0d94b
Edo-Olotu <Ejiroghene.Edo-Olotu@uhs.nhs.uk>
2025-12-29 14:04:10 +0000

    js coding

---
+++ script.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll("button")
+const display = document.getElementById("display")
+
+function extract_num(word) {
+    let number_part = word.slice(3);
+    return parseInt(number_part);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+btns.forEach(button => {
+    button.addEventListener("click", (event) => {
+        let id = event.target.id;
+        let number = extract_num(id)
+        if (numbers.includes(number)) {
+            display.value += number
+        }
+
+        if (id === "all-clear") {
+            display.value = ""
+        }
+
+        
+    })    
+})
+
